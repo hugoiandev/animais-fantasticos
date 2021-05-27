252f4bc196485b9288f0d3df2b786d962c9ea856
Hugo Ian <ahugaao@gmail.com>
2021-05-27 20:26:58 -0300

    acertado caminho da api

--- js/script.js
+++ js/script.js
@@ -38,5 +38,5 @@ const funcionamento = new Funcionamento('[data-semana]', 'aberto')
 funcionamento.init()
 
 
-fetchAnimais('../../animaisapi.json', '.numeros-grid')
+fetchAnimais('./animaisapi.json', '.numeros-grid')
 FetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
